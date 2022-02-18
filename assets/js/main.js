1983f2fcfb554a541367d861242ff5895d0a0a89
DevMohamedElshazly <moshazly416@gmail.com>
2022-02-18 21:38:19 +0200

    Add

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+// * Global variables
+
+const links = document.getElementById("links");
+const list = document.getElementById("list");
+links.onclick = () => {
+  links.classList.toggle("show");
+};
+
+// * Scroll Reveal
